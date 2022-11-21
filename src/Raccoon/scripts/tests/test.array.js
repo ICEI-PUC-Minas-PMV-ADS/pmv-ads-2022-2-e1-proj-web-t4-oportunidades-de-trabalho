d9c912b7ee208c610610e5f3c591f662faced034
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2022-11-21 19:42:18 -0300

    folder change

---
+++ src/Raccoon/scripts/tests/test.array.js
@@ -0,0 +1,22 @@
+let assert = chai.assert;
+
+assert('foo' !== 'bar', 'foo is not bar');
+assert(Array.isArray([]), 'empty arrays are arrays');
+describe('MyClass tests', function () {
+    describe('The class', function () {
+        it('can be instantiated', function () {
+
+        });
+    });
+});
+
+
+describe("Color Code Converter", function() {
+    describe("RGB to Hex conversion", function() {
+      it("converts the basic colors", function() {
+        var redHex   = 1
+        assert(redHex == 1)
+      });
+    });
+
+  });
